c72007c7954902a954e392a3f331ca9c756fed44
Mostafa Rabie Al-aasr <mostafa2310551@gmail.com>
2025-04-22 08:34:07 +0200

    fix current location

--- lib/features/prayer_times/data/repo/prayer_times_repo_imp.dart
+++ lib/features/prayer_times/data/repo/prayer_times_repo_imp.dart
@@ -4,25 +4,21 @@ import 'package:geolocator/geolocator.dart';
 import 'package:islamic_prayer_times/features/prayer_times/data/repo/prayer_times_repo.dart';
 
 import '../../../../core/constants/api_const.dart';
-import '../../../../core/constants/app_const.dart';
 import '../../../../core/error/failure.dart';
 import '../../../../core/network/dio_helper/dio_helper.dart';
-
 import '../models/prayer_times_model.dart';
 
 class PrayerTimesRepoImp extends PrayerTimesRepo {
   @override
   Future<Either<Failure, PrayerTimesModel>> getPrayerTimes() async {
     try {
+      final position = await Geolocator.getCurrentPosition();
 
-      final response = await
-      DioHelper.getData(url:ApiConstant.prayerTimesUrl ,query:{
-        // 'latitude': position.latitude,
-        'latitude': 30.0444,
-        // 'longitude': position.longitude,
-        'longitude': 31.2357,
-      } );
-
+      final response =
+          await DioHelper.getData(url: ApiConstant.prayerTimesUrl, query: {
+        'latitude': position.latitude,
+        'longitude': position.longitude,
+      });
 
       return right(PrayerTimesModel.fromJson(response.data));
     } catch (e) {
@@ -32,6 +28,4 @@ class PrayerTimesRepoImp extends PrayerTimesRepo {
       return left(ServerFailure(e.toString()));
     }
   }
-
-
 }
